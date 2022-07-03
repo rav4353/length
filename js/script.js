d12a74e1560a1936624a91f2946ae5d336969ea2
rav4353 <ravanthsri20@gmail.com>
2022-07-03 11:08:34 +0530

    length

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Length
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits;
+fruits[fruits.length] = "Kiwi";
+document.getElementById("demo2").innerHTML = fruits;
